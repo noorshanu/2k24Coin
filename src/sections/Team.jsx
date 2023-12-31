572ad49937ec99db7e8fbf483b3063753f8e0b3c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-01 04:16:22 +0530

    Update Team.jsx

--- src/sections/Team.jsx
+++ src/sections/Team.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
+import React from "react";
 import Title from "components/Title";
 
 function Team() {
   return (
     <section className="relative z-10 bg-[#ffeeb9] py-4  my-4">
-    <div className="container-wrapper mb-14">
-      <Title className="text-center text-[#000000] font-bold mb-10 pt-8">
-        Meet Team
-      </Title>
+      <div className="container-wrapper mb-14">
+        <Title className="text-center text-[#000000] font-bold mb-10 pt-8">
+          Meet Team
+        </Title>
 
-   
+        <div className="flex justify-evenly flex-col sm:flex-row">
+          <div className=" text-center">
+            <img
+              src="images/team/team1.JPG"
+              alt=""
+              className="h-[300px] border-4 border-[#f4382d] rounded-md"
+            />
+            <h3 className=" font-onep text-lg mt-2">Neko-San</h3>
+            <p className=" text-base">(Deving Cat )</p>
+          </div>
 
-      <div className="flex justify-evenly flex-col sm:flex-row">
-       <img src="images/team/team1.JPG" alt="" className='h-[300px] border-4 border-[#f4382d] rounded-md' />
-       <img src="images/team/team2.JPG" alt=""  className='h-[300px] border-4 border-[#f4382d] rounded-md' />
-       <img src="images/team/team3.JPG" alt=""  className='h-[300px] border-4 border-[#f4382d] rounded-md' />
-      
-     
+          <div className=" text-center">
+            <img
+              src="images/team/team2.JPG"
+              alt=""
+              className="h-[300px] border-4 border-[#f4382d] rounded-md"
+            />
+            <h3 className=" font-onep text-lg mt-2">ZoroinBoots   </h3>
+            <p className=" text-base">(Marketoor )</p>
+          </div>
+
+          <div className="text-center">
+            <img
+              src="images/team/team3.JPG"
+              alt=""
+              className="h-[300px] border-4 border-[#f4382d] rounded-md"
+            />
+            <h3 className=" font-onep text-lg mt-2">NamiNeko
+</h3>
+            <p className=" text-base">(Catty-Cat )</p>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 }
 
-export default Team
+export default Team;
